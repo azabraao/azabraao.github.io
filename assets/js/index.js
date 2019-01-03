5ebeb9e8f2cef343fde63ed840a9a9d45cc5e6ac
Abraão Azevedo <az.abraao@gmail.com>
2019-01-03 13:44:59 -0200

    Improve habilities exibition
    - Uses percentage value instead of pixels \n - Change the media query to get more responsive aproach \n - Order the exibition from the strongest habilities trough Javascript

--- assets/js/index.js
+++ assets/js/index.js
@@ -3,10 +3,14 @@ $(document).ready(function(){
         $(this).find('.portifolio__item-description').toggle();
         $(this).toggleClass("js-unset-height");
     });
-    $('.js-hability-percentage').data("percentage");
-    
-    $( ".js-hability-percentage" ).each(function(index){
+
+    $(".js-hability-percentage" ).each(function(index){
         let percent = $(this).data("percentage");
         $(this).width(percent);
     });
+    
+    habilitie__list_odered = $(".js-hability-percentage" ).sort((a,b) => $(b).data('percentage') - $(a).data('percentage'));
+    
+    $(habilitie__list_odered).each((index,item) => {$(item).parent().css("order",index)});
+
 });
