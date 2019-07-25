6c8bd7b8e3f7882693b7e15d62da174a9a15f1aa
Abraao Azevedo <abraao.azevedo@corebiz.com.br>
2019-07-25 15:42:17 -0300

    Add theme radomization

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,16 +1,43 @@
-$(document).ready(function(){
-    $('.js-portifolio-item').on('click',function(){
-        $(this).find('.portifolio__item-description').toggle();
-        $(this).toggleClass("js-unset-height");
-    });
+let header = document.querySelector('.header');
+let footer = document.querySelector('.footer');
 
-    $(".js-hability-percentage" ).each(function(index){
-        let percent = $(this).data("percentage");
-        $(this).width(percent);
-    });
-    
-    habilitie__list_odered = $(".js-hability-percentage" ).sort((a,b) => $(b).data('percentage') - $(a).data('percentage'));
-    
-    $(habilitie__list_odered).each((index,item) => {$(item).parent().css("order",index)});
+let palletes = [
+    {primary: '#001427', secondary: '#F4D58D'},
+    {primary: '#000000', secondary: '#ffffff'},
+    {primary: '#271F30', secondary: '#D0FCB3'},
+    {primary: '#331E38', secondary: '#A0C1B9'},
+    {primary: '#44355B', secondary: '#ECA72C'},
+    {primary: '#5B7553', secondary: '#C3E8BD'},
+    {primary: '#136F63', secondary: '#E0CA3C'},
+    {primary: '#E0CA3C', secondary: '#3E2F5B'},
+    {primary: '#B9D6F2', secondary: '#0353A4'},
+    {primary: '#424242', secondary: '#FCFC62'},
+];
 
-});
+randomizeTheme();
+
+function randomIndex(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function randomizeTheme() {
+    let index = randomIndex(0, palletes.length -1);
+    
+    if(palleteIsRepeated(index)) {
+        randomizeTheme();
+        return;
+    }
+
+    header.style.backgroundColor = palletes[index].primary
+    header.style.color = palletes[index].secondary
+    footer.style.backgroundColor = palletes[index].secondary
+    footer.style.color = palletes[index].primary
+
+    window.localStorage.setItem('pallete-index', index);
+}
+
+function palleteIsRepeated(palleteIndex) {
+    return  window.localStorage.getItem('pallete-index') == palleteIndex;
+}
