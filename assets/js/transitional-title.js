3865c46fd0660c72e3206da0743501d1393bf668
Abraao Azevedo <abraao.azevedo@corebiz.com.br>
2019-07-31 12:08:37 -0300

    Refactor change title letter by letter feature

--- assets/js/transitional-title.js
+++ assets/js/transitional-title.js
@@ -1,26 +1,51 @@
 let titles = ['Front-end', 'Empreendedor', 'Maker','Desenvolvedor Web'];
 let transitionalTitle = document.querySelector('.jsTransitional');
 
-showTitle(titles[0], 0)
+changeTitle(titles[0], 0)
 
-function showTitle(title, counter = 0) {
+function changeTitle(title, counter = 0) {
+  resetTitle();
+  showLetterByLetter(title, counter);
+}
+
+function resetTitle() {
   transitionalTitle.textContent = "";
-  let letters = title.split('');
-  if(counter === titles.length) {
-    counter = 0;
-  }
-  
-  transitionalTitle.textContent = '';
+}
+
+function showLetterByLetter(string, counter) {
+  let letters = string.split('');
 
   letters.map((letter, index) => {
     setTimeout(() => {
-      transitionalTitle.textContent += letter;
+      addLetterToTitle(letter);
 
-      if(index === letters.length - 1) {
-        setTimeout(() => {
-          showTitle(titles[counter], counter + 1)
-        },1000);
+      if(isLastLetter(index, letters)) {
+        changeTitleRecursion(counter + 1)
       }
-    },100 * index)
+    },increaseSeconds(index))
   });
+}
+
+function addLetterToTitle(letter) {
+  transitionalTitle.textContent += letter;
+}
+
+function increaseSeconds(index) {
+  return 100 * index;
+}
+
+function isLastLetter(index, letters) {
+  return index === letters.length - 1;
+}
+
+function changeTitleRecursion(counter) {
+  if(islastTitle(counter)) counter = 0;
+  
+  setTimeout(() => {
+    changeTitle(titles[counter], counter);
+  },1000);  
+}
+
+function islastTitle(counter) {
+  return counter === titles.length;
 }
