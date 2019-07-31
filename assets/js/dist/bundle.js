d5fd3e78cdd3213d69e3f7a260278db1995bff19
Abraao Azevedo <abraao.azevedo@corebiz.com.br>
2019-07-31 13:07:24 -0300

    Add js bundle

--- assets/js/dist/bundle.js
+++ assets/js/dist/bundle.js
@@ -43,3 +43,54 @@ function randomizeTheme() {
 function palleteIsRepeated(palleteIndex) {
     return  window.localStorage.getItem('pallete-index') == palleteIndex;
 }
+let titles = ['Front-end', 'Empreendedor', 'Maker','Desenvolvedor Web'];
+let transitionalTitle = document.querySelector('.jsTransitional');
+
+changeTitle(titles[0], 0)
+
+function changeTitle(title, counter = 0) {
+  resetTitle();
+  showLetterByLetter(title, counter);
+}
+
+function resetTitle() {
+  transitionalTitle.textContent = "";
+}
+
+function showLetterByLetter(string, counter) {
+  let letters = string.split('');
+
+  letters.map((letter, index) => {
+    setTimeout(() => {
+      addLetterToTitle(letter);
+
+      if(isLastLetter(index, letters)) {
+        changeTitleRecursion(counter + 1)
+      }
+    },increaseSeconds(index))
+  });
+}
+
+function addLetterToTitle(letter) {
+  transitionalTitle.textContent += letter;
+}
+
+function increaseSeconds(index) {
+  return 100 * index;
+}
+
+function isLastLetter(index, letters) {
+  return index === letters.length - 1;
+}
+
+function changeTitleRecursion(counter) {
+  if(islastTitle(counter)) counter = 0;
+  
+  setTimeout(() => {
+    changeTitle(titles[counter], counter);
+  },1000);  
+}
+
+function islastTitle(counter) {
+  return counter === titles.length;
+}
--- gulpfile.js
+++ gulpfile.js
@@ -5,7 +5,8 @@ let sass = require('gulp-sass');
 let uglifycss = require('gulp-uglifycss');
 let rename = require("gulp-rename");
 var sourcemaps = require("gulp-sourcemaps");
-var concat = require("gulp-concat-js");
+var concat = require('gulp-concat');
+var minify = require('gulp-minify');
 
 sass.compiler = require('node-sass');
  
@@ -27,18 +28,15 @@ gulp.task('css', async () => {
     .pipe(gulp.dest('./assets/css'));
 }); 
 
-gulp.task("build", function () {
-  return gulp.src(["./assets/js/*.js"])
-      .pipe(sourcemaps.init())
-        .pipe(concat({
-            "target": "concatenated.js", // Name to concatenate to
-            "entry": "./main.js" // Entrypoint for the application, main module
-                                 // The `./` part is important! The path is relative to
-                                 // whatever gulp decides is the base-path, in this
-                                 // example that is `./lib`
-        }))
-      .pipe(sourcemaps.write())
-      .pipe(gulp.dest("./assets/js/dist"));
+gulp.task('pack-js', function () {    
+  return gulp.src(['assets/js/*.js'])
+      .pipe(concat('bundle.js'))
+      .pipe(minify())
+      .pipe(gulp.dest('assets/js/dist'));
+});
+
+gulp.task('js:watch', async () => {
+  gulp.watch('./assets/js/*.js', gulp.series(['pack-js']));
 });
 
 gulp.task('css:watch', async () => {
@@ -52,6 +50,7 @@ gulp.task('sass:watch', async () => {
 gulp.task('watch', async () => {
   gulp.watch('./assets/scss/**/*.scss', gulp.series(['sass']));
   gulp.watch('./assets/css/index.css', gulp.series(['css']));
+  gulp.watch('./assets/js/*.js', gulp.series(['pack-js']));
 });
 
 gulp.task('default', gulp.series(['watch']));
