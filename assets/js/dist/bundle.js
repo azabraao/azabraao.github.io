09be89bff2f25dfe2790ec68450fa742c8cd1f6b
Abraao Azevedo <abraao.azevedo@corebiz.com.br>
2019-08-02 00:13:58 -0300

    Add on mouse weel scrolling event

--- assets/js/dist/bundle.js
+++ assets/js/dist/bundle.js
@@ -1,3 +1,27 @@
+document.addEventListener('wheel', paralaxing );
+
+let firstSection = document.querySelector('.jsFirstSection');
+let portifolio = document.querySelector('.jsPortifolio');
+
+function paralaxing(event) {
+  if (isScrollingDown(event)) {
+    firstSection.classList.remove('forward');
+    firstSection.classList.add('backward');
+    
+    portifolio.classList.remove('unscrolling');
+    portifolio.classList.add('scrolling');
+  } else {
+    firstSection.classList.remove('backward');
+    firstSection.classList.add('forward');
+   
+    portifolio.classList.remove('scrolling');
+    portifolio.classList.add('unscrolling');
+  }
+}
+
+function isScrollingDown(event) {
+  return event.deltaY === 100;  
+}
 let header = document.querySelector('.header');
 let contact = document.querySelector('.contact');
 let chromeTab = document.querySelector("meta[name=theme-color]");
---
+++ assets/js/portifolio-animation.js
@@ -0,0 +1,24 @@
+document.addEventListener('wheel', paralaxing );
+
+let firstSection = document.querySelector('.jsFirstSection');
+let portifolio = document.querySelector('.jsPortifolio');
+
+function paralaxing(event) {
+  if (isScrollingDown(event)) {
+    firstSection.classList.remove('forward');
+    firstSection.classList.add('backward');
+    
+    portifolio.classList.remove('unscrolling');
+    portifolio.classList.add('scrolling');
+  } else {
+    firstSection.classList.remove('backward');
+    firstSection.classList.add('forward');
+   
+    portifolio.classList.remove('scrolling');
+    portifolio.classList.add('unscrolling');
+  }
+}
+
+function isScrollingDown(event) {
+  return event.deltaY === 100;  
+}
