a27a2bf7c6150f971e5c11da3a61802d60b5b88d
Abraão Azevedo <az.abraao@gmail.com>
2020-10-28 18:19:23 -0300

    Upate titles list

--- assets/js/transitional-title.js
+++ assets/js/transitional-title.js
@@ -1,4 +1,4 @@
-let titles = [' Freelancer',' Front-end', 'n Entrepreneur', ' Maker',' Web Developer'];
+let titles = [' Freelancer',' Front-end developer', 'n Entrepreneur', ' Maker',' Web Developer'];
 let transitionalTitle = document.querySelector('.jsTransitional');
 
 changeTitle(titles[0], 0)
