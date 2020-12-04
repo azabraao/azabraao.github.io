f864921ae9889dc17af333e3945bbf50b7c1df85
azabraao <az.abraao@gmail.com>
2020-12-03 23:22:15 -0300

    remove gulp css unnecessary task

--- gulpfile.js
+++ gulpfile.js
@@ -11,22 +11,22 @@ var minify = require('gulp-minify');
 sass.compiler = require('node-sass');
  
 gulp.task('sass', async () => {
-    gulp.src('./assets/scss/index.scss')
-    .pipe(sass().on('error', sass.logError))
-    .pipe(gulp.dest('./assets/css'));
+    gulp
+      .src("./assets/scss/index.scss")
+      .pipe(sass().on("error", sass.logError))
+      .pipe(
+        uglifycss({
+          uglyComments: true,
+        })
+      )
+      .pipe(
+        rename({
+          suffix: ".min",
+          extname: ".css",
+        })
+      )
+      .pipe(gulp.dest("./assets/css"));
 });
-
-gulp.task('css', async () => {
-  gulp.src('./assets/css/index.css')
-    .pipe(uglifycss({
-      "uglyComments": true
-    }))
-    .pipe(rename({
-      suffix: ".min",
-      extname: ".css"
-    }))
-    .pipe(gulp.dest('./assets/css'));
-}); 
 
 gulp.task('pack-js', function () {    
   return gulp.src(['assets/js/*.js'])
@@ -39,17 +39,12 @@ gulp.task('js:watch', async () => {
   gulp.watch('./assets/js/*.js', gulp.series(['pack-js']));
 });
 
-gulp.task('css:watch', async () => {
-  gulp.watch('./assets/css/index.css', gulp.series(['css']))
-});
-
 gulp.task('sass:watch', async () => {
   gulp.watch('./assets/scss/**/*.scss', gulp.series(['sass']));
 });
 
 gulp.task('watch', async () => {
   gulp.watch('./assets/scss/**/*.scss', gulp.series(['sass']));
-  gulp.watch('./assets/css/index.css', gulp.series(['css']));
   gulp.watch('./assets/js/*.js', gulp.series(['pack-js']));
 });
 
